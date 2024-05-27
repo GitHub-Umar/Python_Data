5fc9e4c1d6171c6d9f3b85fc4fdebe6243e7e47d
Umar <Omarjaved865@gmail.com>
2024-05-27 12:05:27 +0500

    PythonData

--- Enumerates.py
+++ Enumerates.py
@@ -12,3 +12,18 @@ for index,x in enumerate(x):
     if(index==2):
      print('is a Caste')
 
+print("_______________")
+
+fruits=['apple','banana','mango']
+count =1
+for x in fruits:
+    print(count,x)
+    count+=1
+
+print("_______________")
+
+students=['umar','ali','ahmad']
+for count in range(len(students)):
+    print(count+1,students[count])
+
+
